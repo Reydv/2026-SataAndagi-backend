a06c9b3ac6a66dbae4ae7e654ee0c91a52bcba93
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-12 20:24:31 +0700

    feat(db): add db context and set table relationship

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using _2026_SataAndagi_backend.Models;
+
+namespace _2026_SataAndagi_backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Professor> Professors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Enforce Unique Identity Number (NRP/NIP)
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.IdentityNumber)
+            .IsUnique();
+
+        // Configure User -> Student (One-to-One)
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.StudentProfile)
+            .WithOne(s => s.User)
+            .HasForeignKey<Student>(s => s.UserId)
+            .OnDelete(DeleteBehavior.Cascade); // If User deleted, delete Profile
+
+        // Configure User -> Professor (One-to-One)
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.ProfessorProfile)
+            .WithOne(p => p.User)
+            .HasForeignKey<Professor>(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
