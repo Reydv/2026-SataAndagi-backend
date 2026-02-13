c0b0c72e31e70a23c8c494eaef1db85e88c7f044
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-13 13:35:32 +0700

    feat(room): register the seeder

--- src/Program.cs
+++ src/Program.cs
@@ -45,7 +45,9 @@ using (var scope = app.Services.CreateScope())
     var context = services.GetRequiredService<_2026_SataAndagi_backend.Data.ApplicationDbContext>();
     // context.Database.Migrate(); // Optional: Automatically runs migrations
     _2026_SataAndagi_backend.Data.UserSeeder.Seed(context);
+    _2026_SataAndagi_backend.Data.RoomSeeder.Seed(context);
 }
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
