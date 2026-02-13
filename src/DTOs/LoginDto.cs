c2f2fc1eb3b8e2a7eb1cb1e519a9afb65bd871ac
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-13 10:52:47 +0700

    feat(auth): create dto for login

---
+++ src/DTOs/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace _2026_SataAndagi_backend.DTOs;
+
+public class LoginDto
+{
+    public string IdentityNumber { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
