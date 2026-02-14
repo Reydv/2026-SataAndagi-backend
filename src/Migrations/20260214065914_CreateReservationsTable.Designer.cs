61facd222f69fcb91d03a0de4b243f82b3b1b30c
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-14 14:06:16 +0700

    feat(reservation): add migration files

--- src/Migrations/20260214065914_CreateReservationsTable.Designer.cs
+++ src/Migrations/20260214065914_CreateReservationsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _2026_SataAndagi_backend.Data;
 
@@ -11,9 +12,11 @@ using _2026_SataAndagi_backend.Data;
 namespace _2026_SataAndagi_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260214065914_CreateReservationsTable")]
+    partial class CreateReservationsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
